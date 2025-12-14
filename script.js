58b9eff4806d13587b40c115c0ad479acbc701fc
lana-bu <lanabu@umich.edu>
2025-12-14 05:53:43 -0500

    subcondition inputs (may change method for functionality)

--- script.js
+++ script.js
@@ -1,12 +1,53 @@
 if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
+  document.addEventListener('DOMContentLoaded', ready)
 } else {
-    ready()
-} // very important for any site
+  ready()
+}
 
 function ready() {
   loadHTML('header', 'header.html'); // Load header into #header
   loadHTML('footer', 'footer.html'); // Load footer into #footer
+}
+
+function toggleDiv() {
+  value = document.getElementById('select').value;
+  div1 = document.getElementById('one');
+  div2 = document.getElementById('two');
+  div3 = document.getElementById('three');
+  div4 = document.getElementById('four');
+  div5 = document.getElementById('five');
+
+  if (parseInt(value) < 2) {
+    div1.style.visibility = 'hidden';
+    div2.style.visibility = 'hidden';
+    div3.style.visibility = 'hidden';
+    div4.style.visibility = 'hidden';
+    div5.style.visibility = 'hidden';
+  } else if (parseInt(value) == 2) {
+    div1.style.visibility = 'visible';
+    div2.style.visibility = 'visible';
+    div3.style.visibility = 'hidden';
+    div4.style.visibility = 'hidden';
+    div5.style.visibility = 'hidden';
+  } else if (parseInt(value) == 3) {
+    div1.style.visibility = 'visible';
+    div2.style.visibility = 'visible';
+    div3.style.visibility = 'visible';
+    div4.style.visibility = 'hidden';
+    div5.style.visibility = 'hidden';
+  } else if (parseInt(value) == 4) {
+    div1.style.visibility = 'visible';
+    div2.style.visibility = 'visible';
+    div3.style.visibility = 'visible';
+    div4.style.visibility = 'visible';
+    div5.style.visibility = 'hidden';
+  } else if (parseInt(value) == 5) {
+    div1.style.visibility = 'visible';
+    div2.style.visibility = 'visible';
+    div3.style.visibility = 'visible';
+    div4.style.visibility = 'visible';
+    div5.style.visibility = 'visible';
+  }
 }
 
 // Function to load HTML content into a target element
